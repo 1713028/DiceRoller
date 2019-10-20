9cbaf661181615bf8dbf9b58b056a7f65599dd77
1713028 <1713028@brunel.ac.uk>
2019-10-20 01:16:49 +0100

    Level 3 done added score box

--- app/src/main/java/com/example/diceactivity/MainActivity.java
+++ app/src/main/java/com/example/diceactivity/MainActivity.java
@@ -14,13 +14,19 @@ public class MainActivity extends AppCompatActivity {
     private Random rng = new Random();
 
     int number;
+    int guess;
+    boolean working = true;
     EditText numberInput;
     TextView Result;
     TextView RollDisplay;
+    TextView CorrectGuesses;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -29,29 +35,33 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
         RollDisplay = (TextView) findViewById(R.id.RollDisplay);
         Result = (TextView) findViewById(R.id.Result);
+        CorrectGuesses = (TextView) findViewById(R.id.CorrectGuesses);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 number = Integer.valueOf(numberInput.getText().toString());
-                int random = rng.nextInt(6)+1;
 
-                RollDisplay.setText(new StringBuilder().append(random));
+                int random = rng.nextInt(6) + 1;
 
-                if (number == random){
-                    Result.setText("Congratulations");
+
+                    RollDisplay.setText(new StringBuilder().append(random));
+
+                    if (number == random) {
+                        Result.setText("Congratulations");
+                        guess = guess + 1;
+                        CorrectGuesses.setText("Correct Guesses: " + guess);
+                    } else if ((number < 1) && (number > 6)) {
+                        Result.setText("Please enter between 1-6 ");
+                    } else {
+                        Result.setText("");
+
+                    }
+
                 }
-                else if ((number < 1) && (number > 6)){
-                    Result.setText("Please enter between 1-6 ");
-                }
-                else{
-                    Result.setText("");
 
-                }
-
-
-            }
         });
 
     }
