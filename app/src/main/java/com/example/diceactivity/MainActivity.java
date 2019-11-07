305cbc371e7b9eb5dffd76003f2cb61d367b9e3f
1713028 <1713028@brunel.ac.uk>
2019-11-07 00:21:04 +0000

    Level 4 done (Part3a) updated

--- app/src/main/java/com/example/diceactivity/MainActivity.java
+++ app/src/main/java/com/example/diceactivity/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.diceactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -26,22 +28,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         numberInput = (EditText) findViewById(R.id.numberInput);
         Button button = (Button) findViewById(R.id.button);
         Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button2);
+
         RollDisplay = (TextView) findViewById(R.id.RollDisplay);
         Result = (TextView) findViewById(R.id.Result);
         CorrectGuesses = (TextView) findViewById(R.id.CorrectGuesses);
 
 
+            //  Block of code to try
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                number = Integer.valueOf(numberInput.getText().toString());
+                try {
+                    number = Integer.parseInt(numberInput.getText().toString());
 
                 roll_the_dice();
+
 
                 RollDisplay.setText(new StringBuilder().append(roll_the_dice()));
 
@@ -55,10 +63,18 @@ public class MainActivity extends AppCompatActivity {
                     Result.setText("");
 
                 }
-
+                }
+                catch(Exception e)
+                {
+                    Result.setText("PLEASE INPUT ONLY NUMBERS");
+                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                }
             }
 
         });
+
+
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,16 +102,25 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-    }
 
+    button3.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity2();
+        }
 
+    });
 
+}
     public Integer roll_the_dice(){
         int random = rng.nextInt(6) + 1;
         return random;
     }
 
-
+    public void openActivity2() {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
 
 
 }
