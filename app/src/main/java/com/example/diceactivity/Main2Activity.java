3b1f4b131d5d618ffcda458805e08fc5a39dd1fb
1713028 <1713028@brunel.ac.uk>
2019-11-08 03:08:25 +0000

    Level 4 done (Part3a) updated with api

--- app/src/main/java/com/example/diceactivity/Main2Activity.java
+++ app/src/main/java/com/example/diceactivity/Main2Activity.java
@@ -1,14 +1,76 @@
 package com.example.diceactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import allbegray.slack.SlackClientFactory;
+import allbegray.slack.webapi.SlackWebApiClient;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class Main2Activity extends AppCompatActivity {
+
+    final String slackToken = "xoxp-763082602722-785364129810-812593245186-06d220f8d25bf781fbbee280e63fc1eb";
+
+    SlackWebApiClient mWebApiClient = SlackClientFactory.createWebApiClient(slackToken);
+
+    Button ShareScore = (Button) findViewById(R.id.ShareScore);
+
+
+
+
+    private class SlackAPITake extends AsyncTask<Void, Void, Void>
+    {
+        protected Void doInBackground (Void... voids) {
+            Intent intent2 = getIntent();
+            String message = intent2.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            mWebApiClient.postMessage("CP4356CUT","Your score is " + message);
+            return null;
+
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
+
+
+    public void ShareScore(View v){
+        try {
+            TextView tv = this.findViewById(R.id.textView);
+            enableStrictMode();
+            Intent intent2 = getIntent();
+            String message = intent2.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            tv.setText("your score: " + message + "is shared on Slack!");
+            new SlackAPITake().execute();
+        }
+        catch(Exception ex){
+            Toast.makeText(Main2Activity.this, "error server not responding" + ex.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("MainActivity:Error", "Error on button click, see the log", ex);
+        }
+    }
+
+    private void enableStrictMode() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void back (View v){
+        Intent BackButton = new Intent (this, MainActivity.class);
+        startActivity(BackButton);
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/diceactivity/MainActivity.java
+++ app/src/main/java/com/example/diceactivity/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.diceactivity;
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.ArrayList;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,12 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
     int number;
     int guess;
-    boolean working = true;
     EditText numberInput;
     TextView Result;
     TextView RollDisplay;
     TextView CorrectGuesses;
 
+    public static final String EXTRA_MESSAGE = "extra_message";
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
@@ -31,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
         numberInput = (EditText) findViewById(R.id.numberInput);
         Button button = (Button) findViewById(R.id.button);
         Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button2);
-
+        Button button3 = (Button) findViewById(R.id.button3);
         RollDisplay = (TextView) findViewById(R.id.RollDisplay);
         Result = (TextView) findViewById(R.id.Result);
         CorrectGuesses = (TextView) findViewById(R.id.CorrectGuesses);
@@ -78,30 +77,30 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch(rng.nextInt(6)+1) {
-                    case 1:
-                        Result.setText("If you could go anywhere in the world, where would you go?\n");
-                        break;
-                    case 2:
-                        Result.setText("If you were stranded on a desert island, what three things would you want to take with you?\n");
-                        break;
-                    case 3:
-                        Result.setText("If you could eat only one food for the rest of your life, what would that be?\n");
-                        break;
-                    case 4:
-                        Result.setText("If you won a million dollars, what is the first thing you would buy?\n");
-                        break;
-                    case 5:
-                        Result.setText("If you could spend the day with one fictional character, who would it be?\n");
-                        break;
-                    case 6:
-                        Result.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?\n");
-                        break;
-                    default:
-                }
+
+                int number;
+
+                number = roll_the_dice();
+
+
+
+
+                ArrayList<String> DiceBreaker = new ArrayList<String>();
+                DiceBreaker.add("If you could go anywhere in the world, where would you go?\n");
+                DiceBreaker.add("If you were stranded on a desert island, what three things would you want to take with you?\n");
+                DiceBreaker.add("If you could eat only one food for the rest of your life, what would that be?\n");
+                DiceBreaker.add("If you won a million dollars, what is the first thing you would buy?\n");
+                DiceBreaker.add("If you could spend the day with one fictional character, who would it be?\n");
+                DiceBreaker.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?\n");
+
+                Result.setText(DiceBreaker.get(number));
+
+
             }
 
         });
+
+
 
     button3.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -110,17 +109,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
     });
-
 }
+
     public Integer roll_the_dice(){
         int random = rng.nextInt(6) + 1;
         return random;
     }
 
+
     public void openActivity2() {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
+
+
 
 
 }
